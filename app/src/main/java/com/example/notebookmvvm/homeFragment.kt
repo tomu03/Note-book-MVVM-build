53ea2c3d9e77a41968843cf85700a942d3557678
tomu03 <369tomu@gamil.com>
2024-05-13 13:18:24 +0600

    Home fragment and New Note fragment added and connected to the adapter

--- app/src/main/java/com/example/notebookmvvm/homeFragment.kt
+++ app/src/main/java/com/example/notebookmvvm/homeFragment.kt
@@ -5,15 +5,56 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.notebookmvvm.Adapter.NoteAdapter
+import com.example.notebookmvvm.Repository.NoteRepository
+import com.example.notebookmvvm.database.NoteDatabase
+import com.example.notebookmvvm.databinding.FragmentHomeBinding
+import com.example.notebookmvvm.viewmodel.NoteViewModel
+import com.example.notebookmvvm.viewmodel.NoteViewModelFactory
+
 
 class homeFragment : Fragment() {
 
+    private lateinit var noteViewModel: NoteViewModel
+    private lateinit var noteAdapter: NoteAdapter
+    private lateinit var noteRepository: NoteRepository
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val noteDatabase = NoteDatabase(requireContext())
+        val repository = NoteRepository(noteRepository)
+        val factory =  NoteViewModelFactory(requireActivity().application,repository)
+        noteViewModel = ViewModelProvider(this,factory).get(NoteViewModel::class.java)
+
+        val adapter = NoteAdapter()
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.adapter = adapter
+
+        noteViewModel.getAllNotes().observe(viewLifecycleOwner,{
+            notes -> notes?.let {
+                adapter.differ.submitList(it)
+            if (it.isEmpty()) {
+                binding.recyclerView.visibility = View.GONE
+                binding.noNoteAvailable.visibility = View.VISIBLE
+            } else {
+                binding.recyclerView.visibility = View.VISIBLE
+                binding.noNoteAvailable.visibility = View.GONE
+            }
+        }
+        })
+
+        binding.faBtn.setOnClickListener{
+            val action = homeFragmentDirections.ActionHomeFragmentToUpdateNoteFragment()
+            findNavController().navigate(action)
+        }
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/notebookmvvm/newNoteFragment.kt
+++ app/src/main/java/com/example/notebookmvvm/newNoteFragment.kt
@@ -3,16 +3,62 @@ package com.example.notebookmvvm
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.example.notebookmvvm.Repository.NoteRepository
+import com.example.notebookmvvm.database.NoteDatabase
+import com.example.notebookmvvm.databinding.FragmentNewNoteBinding
+import com.example.notebookmvvm.model.Note
+import com.example.notebookmvvm.viewmodel.NoteViewModel
+import com.example.notebookmvvm.viewmodel.NoteViewModelFactory
 
 class newNoteFragment : Fragment() {
+    private lateinit var binding: FragmentNewNoteBinding
+    private lateinit var noteViewModel: NoteViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_note, container, false)
+        binding = FragmentNewNoteBinding.inflate(inflater, container, false)
+        val noteDatabase = NoteDatabase(requireContext())
+        val repository = NoteRepository(noteDatabase)
+        val factory = NoteViewModelFactory(requireActivity().application, repository)
+        noteViewModel = ViewModelProvider(this, factory).get(NoteViewModel::class.java)
+        setHasOptionsMenu(true)
+        return binding.root
     }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_new_note, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_save -> {
+                saveNote()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun saveNote() {
+        val noteTitle = binding.noteTitleEt.text.toString().trim()
+        val noteBody = binding.noteBodyEt.text.toString().trim()
+
+        if (noteTitle.isNotEmpty() && noteBody.isNotEmpty()) {
+            val note = Note(noteTitle =  noteTitle, noteBody = noteBody)
+            noteViewModel.addNote(note)
+            requireActivity().onBackPressed()
+        } else {
+            Toast.makeText(requireContext(), "Please enter a note title and body", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
 }
